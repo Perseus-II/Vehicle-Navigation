9c685522928737b4e11c8b1483247c637dad14f6
Ethan Hayon <ehayon@gmail.com>
2013-11-14 19:39:07 -0500

    update global dst values

--- src/dst/init_dst.cpp
+++ src/dst/init_dst.cpp
@@ -1,5 +1,6 @@
 #include "../../lib/dst.h"
 #include "../../lib/perseus.h"
+#include "../extern.h"
 
 #define MAX 255
 
@@ -10,15 +11,49 @@ void connect_to_dst();
 int dst_fd = -1;
 
 void *init_dst(void *data) {
-	char buf[MAX];
+	char c;
+	char line[MAX];
+	char *pch;
+	int i;
 	int n;
+	float depth_ft, depth_m, temp;
 
 	while(1) {
 		if(dst_fd < 0) connect_to_dst();
 	
-		if((n=read(dst_fd, buf, MAX)) > 0) {
-			printf("Read %s from DST\n", buf);
-		}	
+		i = 0;
+		bzero(line,MAX);
+		while((n=read(dst_fd,&c,1)) > 0) {
+			if(c == '\n') {
+				/* line complete, do something with it */
+				pch = strtok(line, ",");		
+				if(pch != NULL) {
+					/* $SDDBT,1.5,f,0.4,M,0.2,F*04 */
+					if(strcmp(pch, "$SDDBT") == 0) {
+						/* depth from bottom */
+						pch = strtok(NULL, ",");
+						depth_ft = atof(pch);	
+						pch = strtok(NULL, ",");
+						pch = strtok(NULL, ",");
+						depth_m = atof(pch);	
+						pthread_mutex_lock(&dst_mutex);
+						vehicle_dst -> depth_ft = depth_ft;
+						vehicle_dst -> depth_m = depth_m;
+						pthread_mutex_unlock(&dst_mutex);
+					} else if(strcmp(pch, "$YXMTW") == 0) {
+						/* water temperature */
+						/* $YXMTW,28.1,C*19 */
+						pch = strtok(NULL, ",");
+						temp = atof(pch);
+						pthread_mutex_lock(&dst_mutex);
+						vehicle_dst -> temperature = temp;
+						pthread_mutex_unlock(&dst_mutex);
+					}
+				}
+				break;
+			}
+			line[i++] = c;	
+		}
 	}
 }
 
